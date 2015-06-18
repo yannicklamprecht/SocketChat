505f7993241cf81b224e14207bcc4feb22b094a7
Yannick Lamprecht <yannicklamprecht@live.de>
2015-06-19 01:34:41 +0200

    Implemented and tested EventSystem
    //TODO
    - implement Socketconnection with object-/packetbased transfer
    - implement encryption/decryption XOR

--- ChatLib/src/de/thm/iem/ylmp88/chatlib/connection/Paket.java
+++ ChatLib/src/de/thm/iem/ylmp88/chatlib/connection/Paket.java
@@ -1,9 +1,12 @@
 package de.thm.iem.ylmp88.chatlib.connection;
 
+import de.thm.iem.yslmp88.chatlib.eventsystem.Event;
+
 import java.io.Serializable;
 
 /**
  * Created by yannick on 19.06.2015.
  */
-public class Paket implements Serializable {
+public class Paket extends Event implements Serializable {
+    //TODO use working client/Server Object connection with eventsystem
 }
--- ChatLib/src/de/thm/iem/yslmp88/chatlib/eventsystem/Event.java
+++ ChatLib/src/de/thm/iem/yslmp88/chatlib/eventsystem/Event.java
@@ -3,5 +3,5 @@ package de.thm.iem.yslmp88.chatlib.eventsystem;
 /**
  * Created by yannick on 02.06.2015.
  */
-public class Event {
+public class Event{
 }
